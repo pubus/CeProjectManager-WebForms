502c64e7c6de5d997122adde68a688f908b91f61
pubus <pubus.trigger.happ>
2015-04-15 15:40:39 +0200

    News

---
+++ CeProjectManager/AddNews.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CeProjectManager.Models;
+
+namespace CeProjectManager
+{
+    public partial class AddNews : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (CeSystemContext db = new CeSystemContext())
+            {
+                List<Group> groups = db.Groups.ToList();
+                
+                foreach (Group g in groups)
+                {
+                    GroupDropDownList.Items.Add(new ListItem(g.Name));    
+                }
+            }
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            using (CeSystemContext db = new CeSystemContext())
+            {
+                News newNews = new News()
+                {
+                    Subject = NewsSubject.Value,
+                    Text = NewsTextBox.Text,
+                    //Get group by name
+                    DestinationGroup = db.Groups.Where(g => g.Name == GroupDropDownList.SelectedItem.Value).ToList()[0],
+                    DateTime = DateTime.UtcNow
+                };
+
+                db.News.Add(newNews);
+                db.SaveChanges();
+            }
+
+            Response.Redirect("ViewNews.aspx");
+        }
+    }
+}
--- CeProjectManager/ViewNews.aspx
+++ CeProjectManager/ViewNews.aspx
@@ -2,4 +2,6 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <div ID="NewsContener" runat="server" class="col-lg-12">
     </div>
+    <a type="button" class="btn btn-primary" href="AddNews.aspx">Add News</a> 
+
 </asp:Content>
